de0eca8790d14e496e18a17789b09acaeb3189b8
TranVinh-ds <chenvinh2003@gmail.com>
2023-07-05 07:17:10 +0700

    new design for login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,38 +18,45 @@ const Login = () => {
     navigate('/dashboard');
   };
   return (
-    <section className="section-login">
-      <h4 className="section-title">Login</h4>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <div className="login-form-row">
-          <label htmlFor="name" className="login-form-label">
-            Name
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="login-form-input"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="login-form-row">
-          <label htmlFor="name" className="login-form-label">
-            Email
-          </label>
-          <input
-            id="name"
-            type="email"
-            className="login-form-input"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="login-btn">
-          submit
-        </button>
-      </form>
-    </section>
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h4
+        style={{
+          textAlign: 'center',
+          marginBottom: '2rem',
+          textTransform: 'capitalize',
+          fontSize: '1.5rem',
+        }}
+      >
+        login
+      </h4>
+      <div className="login-form-row">
+        <label htmlFor="name" className="login-form-label">
+          Name
+        </label>
+        <input
+          id="name"
+          type="text"
+          className="login-form-input"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="login-form-row">
+        <label htmlFor="name" className="login-form-label">
+          Email
+        </label>
+        <input
+          id="name"
+          type="email"
+          className="login-form-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button type="submit" className="login-btn login-btn-block">
+        submit
+      </button>
+    </form>
   );
 };
 
